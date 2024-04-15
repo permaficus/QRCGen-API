4c3830fca7f6a29e903e9cdc757a051b28196f2c
permaficus <abukhalif2019@gmail.com>
2024-04-15 18:06:24 +0700

    chore(middleware): add request validation

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,9 +1,10 @@
 import Router from 'express'
 import { handleIncommingRequest } from '../controller';
+import { validateRequest } from '../middleware/requestValidation';
 export const router = Router();
 
 /**
  * route only handle POST method
  */
 
-router.post('/generate', handleIncommingRequest)
+router.post('/generate', validateRequest, handleIncommingRequest)
