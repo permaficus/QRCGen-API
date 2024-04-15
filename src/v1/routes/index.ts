41e6cb8a64c5daeeb8bfd0b91926826f48916b19
permaficus <abukhalif2019@gmail.com>
2024-04-15 16:52:00 +0700

    chore(routes); add new routes

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,3 +1,9 @@
 import Router from 'express'
-
+import { handleIncommingRequest } from '../controller';
 export const router = Router();
+
+/**
+ * route only handle POST method
+ */
+
+router.post('/generate', handleIncommingRequest)
