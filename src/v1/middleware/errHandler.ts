c7bb72c4e7f848e40a3bcd2f9065fe0a26c4dc1d
permaficus <abukhalif2019@gmail.com>
2024-04-16 15:03:15 +0700

    chore: update error handling on JSON error format

--- src/v1/middleware/errHandler.ts
+++ src/v1/middleware/errHandler.ts
@@ -1,5 +1,15 @@
 import { NextFunction, Request, Response } from "express";
 
+export const jsonError = async (err: any, req: Request, res: Response, next: NextFunction) => {
+    if (err instanceof SyntaxError && 'body' in err) {
+        res.status(400).send({
+            status: 'ERR_BAD_REQUEST',
+            code: res.statusCode,
+            details: `Oops..there is something wrong with your request. We cant process it`
+        }).end();
+        return;
+    }
+}
 export const pathNotFound = async (req: Request, res: Response, next: NextFunction) => {
     res.status(404);
     next(new Error(`Path Not Found - ${req.originalUrl}`));
