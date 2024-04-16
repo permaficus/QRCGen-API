0b050dd1f08119c719fcc8964de9ca9ee7ce1a4d
permaficus <abukhalif2019@gmail.com>
2024-04-16 11:53:02 +0700

    chore(middleware): add global errHandler for routes

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,10 +1,13 @@
 import Router from 'express'
 import { handleIncommingRequest } from '../controller';
 import { validateRequest } from '../middleware/requestValidation';
+import { errHandler, pathNotFound } from '../middleware/errHandler';
 export const router = Router();
 
 /**
  * route only handle POST method
  */
 
-router.post('/generate', validateRequest, handleIncommingRequest)
+router.post('/generate', validateRequest, handleIncommingRequest);
+router.use(pathNotFound)
+router.use(errHandler)
