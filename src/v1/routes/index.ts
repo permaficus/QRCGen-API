9691887c6bbfa3475ce44ca6eb581b9dc1f98bd5
permaficus <abukhalif2019@gmail.com>
2024-05-12 22:20:09 +0700

    chore: update on handling http error

--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,7 +1,6 @@
 import Router from 'express'
 import { handleIncommingRequest } from '@/v1/controller';
 import { validateRequest } from '@/v1/middleware/requestValidation';
-import { errHandler, pathNotFound } from '@/v1/middleware/errHandler';
 export const router = Router();
 
 /**
@@ -9,5 +8,3 @@ export const router = Router();
  */
 
 router.post('/generate', validateRequest, handleIncommingRequest);
-router.use(pathNotFound)
-router.use(errHandler)
